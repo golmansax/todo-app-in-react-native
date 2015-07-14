cb8d059d65a52be36474596b30b54bd45cffc8b1
Holman Gao <holman@golmansax.com>
2015-07-13 22:36:34 -0700

    Throw in NavigatorIOS

--- index.ios.js
+++ index.ios.js
@@ -1,102 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  Image,
-  ListView,
-  View,
+  NavigatorIOS,
 } = React;
-var { getAll } = require('./todos/store');
+var TodoIndexRoute = require('./todos/index_route');
 
-
-var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2
-      }),
-      loaded: false,
-    };
-  },
-
-  componentDidMount: function () {
-    setTimeout(() => {
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(getAll()),
-        loaded: true,
-      });
-    }, 500);
-  },
-
-  render: function () {
-    if (!this.state.loaded) {
-      return this._renderLoadingView();
-    }
-
+class TodoApp extends React.Component {
+  render() {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this._renderTodo}
-        style={styles.listView}
+      <NavigatorIOS
+        style={{ flex: 1 }}
+        initialRoute={{
+          title: 'Todo List',
+          component: TodoIndexRoute,
+        }}
       />
     );
-  },
-
-  _renderTodo: function (todo) {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.thumbnail} source={{ uri: todo.image }} />
-        <View style={styles.rightContainer}>
-          <Text style={styles.name}>{todo.name}</Text>
-          <Text style={styles.date}>{todo.date}</Text>
-        </View>
-      </View>
-    );
-  },
-
-  _renderLoadingView: function () {
-    return (
-      <View style={styles.container}>
-        <Text>Loading todos...</Text>
-      </View>
-    );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
-    flex: 1
-  },
-  name: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginBottom: 8,
-  },
-  date: {
-    textAlign: 'center'
-  },
-  thumbnail: {
-    width: 50,
-    height: 50
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
-});
+}
 
 AppRegistry.registerComponent('TodoApp', () => TodoApp);
---
+++ todos/index_route.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  Image,
+  ListView,
+  View,
+} = React;
+var Button = require('react-native-button');
+var { getAll } = require('./store');
+
+class TodosIndexRoute extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(getAll()),
+        loaded: true,
+      });
+    }, 500);
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this._renderLoadingView();
+    }
+
+    return (
+      <View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderTodo}
+          style={styles.listView}
+        />
+        <Button style={styles.addButton}>
+          Add Todo
+        </Button>
+      </View>
+    );
+  }
+
+  _renderTodo(todo) {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.thumbnail} source={{ uri: todo.image }} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.name}>{todo.name}</Text>
+          <Text style={styles.date}>{todo.date}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  _renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading todos...</Text>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 8,
+  },
+  date: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 50,
+    height: 50,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+  addButton: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'red',
+    color: 'white',
+  },
+});
+
+module.exports = TodosIndexRoute;
